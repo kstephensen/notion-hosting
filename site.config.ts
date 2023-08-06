c782a2ee600979b8faff506b4c060e4f2b670495
Your Name <you@example.com>
2023-08-05 21:15:36 -0700

    Added newsletter

--- site.config.ts
+++ site.config.ts
@@ -20,7 +20,7 @@ export default siteConfig({
   github: 'kstephensen',
   linkedin: 'kasenpoint',
   // mastodon: '#', // optional mastodon profile URL, provides link verification
-  // newsletter: '#', // optional newsletter URL
+  newsletter: 'kstephensen', // optional newsletter URL
   // youtube: '#', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
 
   // default notion icon and cover images for site-wide consistency (optional)
