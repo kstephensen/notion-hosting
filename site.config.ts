c159015fe2ef9211aec1877bc8388ea9c7e201ce
Your Name <you@example.com>
2023-08-09 20:03:24 -0700

    Removed search bar

--- site.config.ts
+++ site.config.ts
@@ -37,6 +37,9 @@ export default siteConfig({
   // environment variables. see the readme for more info
   isRedisEnabled: false,
 
+  isSearchEnabled: false,
+
+
   // map of notion page IDs to URL paths (optional)
   // any pages defined here will override their default URL paths
   // example:
